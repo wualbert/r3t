f7b3b878a18d351a049bd007e89d4763e4fbf76c
albertwu <wualbert@mit.edu>
2019-04-12 04:51:51 -0400

    more test cases

--- dubin_car/tests/test_dubin_car.py
+++ dubin_car/tests/test_dubin_car.py
@@ -34,26 +34,26 @@ def test_fixed_very_boxy_world():
         return
     visualize_tree(rrt, world)
 
-# def test_fixed_box_ring_world():
-#     root = np.asarray([1,0.5])
-#     goal = np.asarray([9,9])
-#     world_bound = AABB([(0,0),(10,10)])
-#     world = DC_Map(world_bound)
-#     box1=AABB([(3,4.5),(5,9)])
-#     box2=AABB([(7,2),(9,5)])
-#     box3 = AABB([(1, 1), (2, 3)])
-#     box4 = AABB([(3, 0.5), (6, 4)])
-#     box5 = AABB([(6, 6), (9, 8.5)])
-#     obstacles = [box1, box2,box3,box4,box5]
-#     for obs in obstacles:
-#         world.add_obstacle(obs)
-#     rrt = DC_RGRRTStar(root,world.compute_reachable_set, world.random_sampler, rewire_radius=4)
-#     goal_node = rrt.build_tree_to_goal_state(goal,stop_on_first_reach=True, allocated_time=np.inf)
-#     if goal_node is None:
-#         print('No path found!')
-#         return
-#     visualize_tree(rrt, world)
+def test_fixed_ring_boxy_world():
+    root = np.asarray([0,-37,0])
+    goal = np.asarray([0,37,np.pi*3/4])
+    world_bound = AABB([(-50,-50),(50,50)])
+    world = DC_Map(world_bound)
+    box1=AABB([(-35,-35),(35,35)])
+    box2=AABB([(-50,-50),(-40,50)])
+    box3 = AABB([(40, -50), (50, 50)])
+    box4 = AABB([(-50, -50), (50, -40)])
+    box5 = AABB([(-50,40),(50,50)])
+    obstacles = [box1, box2,box3,box4,box5]
+    for obs in obstacles:
+        world.add_obstacle(obs)
+    rrt = DC_RGRRTStar(root,world.compute_reachable_set, world.random_sampler, rewire_radius=4)
+    goal_node = rrt.build_tree_to_goal_state(goal,stop_on_first_reach=True, allocated_time=np.inf)
+    if goal_node is None:
+        print('No path found!')
+        return
+    visualize_tree(rrt, world)
 
 if __name__ == '__main__':
-    test_fixed_very_boxy_world()
+    test_fixed_ring_boxy_world()
     plt.show()
