7b51c41275335b99625094d61e90c12dd1f54724
albertwu <wualbert@mit.edu>
2019-08-31 17:05:30 -0400

    updated polytope distance function

--- symbolic_system/examples/test_pendulum.py
+++ symbolic_system/examples/test_pendulum.py
@@ -4,7 +4,7 @@ from timeit import default_timer
 from polytope_symbolic_system.examples.pendulum import Pendulum
 from rg_rrt_star.symbolic_system.symbolic_system_rg_rrt_star import SymbolicSystem_RGRRTStar
 from pypolycontain.visualization.visualize_2D import visualize_2D_AH_polytope
-from pypolycontain.lib.AH_polytope import distance_point
+from pypolycontain.lib.operations import distance_point_polytope
 from rg_rrt_star.utils.visualization import visualize_node_tree_2D
 import time
 from datetime import datetime
@@ -34,7 +34,7 @@ def test_pendulum_planning():
         return rnd
 
     def contains_goal_function(reachable_set, goal_state):
-        distance, projection = distance_point(reachable_set.polytope_list, goal_state)
+        distance, projection = distance_point_polytope(reachable_set.polytope_list, goal_state)
         if abs(projection[0]-goal_state[0])%(2*np.pi)<1e-1 and abs(projection[1]-goal_state[1])<1e-1:
             return True
         return False
